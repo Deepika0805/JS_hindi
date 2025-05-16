792d5eff1bb2db4ebe1974e23c15d30a18f49d3e
Deepika0805 <kohlipriya6789@gmail.com>
2025-05-16 14:45:44 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144552
+let accountEmail ="deepika@google.com"
+var accountPassword="12345"
+accountCity ="Agra"
+let accountState;
+
+accountEmail ="deep.com"
+accountPassword ="2121"
+accountCity="Kanpur"
+console.log(accountId);
+console .table([accountId, accountEmail, accountPassword, accountCity , accountState])
