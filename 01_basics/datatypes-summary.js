83ca85a0090926bdc811e55437a8f728e5df1bcd
Deepika0805 <kohlipriya6789@gmail.com>
2025-05-18 19:41:29 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// primitive 
+
+// 7 types: String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue= 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+
+// const bigNumber = 23454546566788792323n
+
+
+
+//Reference(Non primitive)
+
+// Array, Objects, Functions 
+
+const heros=["shaktiman","maharaj","doga"];
+let myObj={
+    name: "Deepika",
+    Age: 20,
+}
+
+const myFunction  = function (){
+    console.log("Hello World");
+    
+}
+console.log(typeof anotherId);
