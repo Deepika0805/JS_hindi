111447941ef37a8efeb60a39081af53ade355598
Deepika0805 <kohlipriya6789@gmail.com>
2025-05-18 20:24:22 +0000

    modified file

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId=Symbol('123')
 
-console.log(id===anotherId);
+//console.log(id===anotherId);
 
 // const bigNumber = 23454546566788792323n
 
@@ -29,7 +29,34 @@ let myObj={
 }
 
 const myFunction  = function (){
-    console.log("Hello World");
+    //console.log("Hello World");
     
 }
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
+
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap (Non- primitive)
+
+let myYoutubename ="Deepikadotcom"
+
+let anothername= myYoutubename
+anothername = "chai or code"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne={
+    email: "user@google.com",
+    upi:"user@ybl"}
+
+let userTwo=userOne
+
+userTwo.email="Deepika@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
