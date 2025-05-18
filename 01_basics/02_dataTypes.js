ca568ede460918d48f9c9f94f3578ed51019ef09
Deepika0805 <kohlipriya6789@gmail.com>
2025-05-18 17:27:56 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all JS code as newer version
+
+//alert("3 + 3") // we are using Nodejs, not browser 
+
+console.log(3 + 3) // code readability should be high 
+console.log("Deepika")
+  
+
+let name="Deepika"
+let age=20
+let isLoggedIn=false
+let state = null 
+
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=> standalone value 
+//undefined=>
+//symbol=> unique 
+
+//object
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); // object 
